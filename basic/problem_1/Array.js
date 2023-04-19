e5becbf0259611e2bcbd6c6549dd9553d1c0066b
SangA77 <tkddk1771@gmail.com>
2023-04-19 12:26:08 +0900

    04-19

--- basic/problem_1/Array.js
+++ basic/problem_1/Array.js
@@ -35,15 +35,15 @@
 //   console.log(input_data); // [11,12,13,14,15]
 // }
 
-// {
-//   // forEach 사용
-//   // 배열(이터러블)객체들을 모두 순회를 하기위해 사용을 한다
-//   let input_data = [10, 20, 30, 40, 50];
-//   input_data.forEach((value, index, arr) => {
-//     arr[index] = value * 3;
-//   });
-//   console.log(input_data); // [5, 10, 15, 20, 25]
-// }
+{
+  //   // forEach 사용
+  //   // 배열(이터러블)객체들을 모두 순회를 하기위해 사용을 한다
+  let input_data = [10, 20, 30, 40, 50];
+  let result = [];
+
+  input_data.forEach((item) => result.push(item / 2));
+  console.log(result); // [5, 10, 15, 20, 25]
+}
 
 // {
 // map 사용
@@ -65,11 +65,11 @@
 //   console.log(output_data); // [30, 40]
 // }
 
-{
-  let input_data = [10, 20, 30, 40, 50];
-  input_data.splice(2, 2);
-  console.log(input_data); // [10, 20, 50]
-}
+// {
+//   let input_data = [10, 20, 30, 40, 50];
+//   input_data.splice(2, 2);
+//   console.log(input_data); // [10, 20, 50]
+// }
 
 // {
 //   let input_data = [10, 20, 30, 40, 50];
@@ -77,28 +77,28 @@
 //   console.log(output_data); // 3 *인덱스 번호
 // }
 
-{
-  let input_data = "오늘 날씨는 매우 맑음";
-  let output_data = input_data.split(" ");
-  // 1. 배열의 인덱스를 이용해서 변경하는 방법
-  // output_data[3] = "흐림";
+// {
+//   let input_data = "오늘 날씨는 매우 맑음";
+//   let output_data = input_data.split(" ");
+// 1. 배열의 인덱스를 이용해서 변경하는 방법
+// output_data[3] = "흐림";
 
-  // 2. splice() 메소드 사용하는 방법
-  output_data.splice(3, 1, "흐림");
-  console.log(output_data); // ["오늘", "날씨는", "매우", "흐림"]
-}
+// 2. splice() 메소드 사용하는 방법
+//   output_data.splice(3, 1, "흐림");
+//   console.log(output_data); // ["오늘", "날씨는", "매우", "흐림"]
+// }
 
-{
-  // '=' 금지
-  let input_data = "오늘 날씨는 매우 맑음";
-  let output_data = input_data.split(" ");
-  // console.log(output_data);
-  /* 
+// {
+// '=' 금지
+// let input_data = "오늘 날씨는 매우 맑음";
+// let output_data = input_data.split(" ");
+// console.log(output_data);
+/* 
   *주의* 
   .splice(0, 0, "내일"); 의 결과는 [ '내일', '오늘', '날씨는', '매우', '맑음' ]
   splice(배열의 변경을 시작할 index, 배열에서 제거할 요소의 수, 배열에 추가할 요소)
   <배열에서 제거할 요소의 수는 배열의 변경을 시작할 인덱스부터 제거할 요소의 수를 의미함>
   */
-  output_data.splice(0, 1, "내일");
-  console.log(output_data); // ["내일", "날씨는", "매우", "맑음]
-}
+//   output_data.splice(0, 1, "내일");
+//   console.log(output_data); // ["내일", "날씨는", "매우", "맑음]
+// }
