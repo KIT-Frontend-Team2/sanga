9278b09f4fdcf5cee61762d1f9b520aaf52e3730
SangA77 <tkddk1771@gmail.com>
2023-05-01 02:37:49 +0900

    05-01

--- week05/라우터/todomaster/todolist/src/App.js
+++ week05/라우터/todomaster/todolist/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import router from "./routes/routing";
+import { BrowserRouter, RouterProvider, Route, Routes } from "react-router-dom";
+import WebPage from "./pages/main";
+import WebToonPage from "./pages/webtoon/webtoon";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WebPage />} />
+        <Route path="/web/:webId" element={<WebToonPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
