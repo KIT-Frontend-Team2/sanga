171310d09444e05eebf525b3aade3b0369c3a306
SangA77 <tkddk1771@gmail.com>
2023-05-16 19:59:07 +0900

    삭제

--- week03/Javascript/level1/level.js
+++
@@ -1,4 +0,0 @@
-import { BANK_LIST, ACCOUNT_FORM } from './account.js';
-
-console.log(BANK_LIST);
-console.log(ACCOUNT_FORM);
--- week03/Javascript/디스트럭처링할당.js
+++
@@ -1,4 +0,0 @@
-let x, y, z;
-
-
-const [count, setCount] = useState(0); // 디스트럭처링 할당
